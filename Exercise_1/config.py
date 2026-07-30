8435939c4e93113a1d434ee3940142bbd87f948d
Nitin K <kendre.nitin2000@gmail.com>
2026-07-30 20:16:49 +0530

    updated config.py for more constants.

--- Exercise_1/config.py
+++ Exercise_1/config.py
@@ -1,5 +1,16 @@
+from pathlib import Path
+
 
 class Config:
+    """Configuration constants for download URIs and local paths.
+
+    Attributes:
+        DOWNLOAD_URI (tuple): Remote ZIP file URLs to download.
+        EXTRACT_PATH (str): Local path where ZIP contents are extracted.
+        DOWNLOAD_PATH (str): Local path where ZIP files are stored.
+
+    """
+
     DOWNLOAD_URI = (
         "https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2018_Q4.zip",
         "https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2019_Q1.zip",
@@ -9,3 +20,6 @@ class Config:
         "https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2020_Q1.zip",
         "https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2220_Q1.zip",
     )
+
+    EXTRACT_PATH = DOWNLOAD_PATH = Path("downloads")
+    LOGGER_NAME = "download_and_extract_files"
