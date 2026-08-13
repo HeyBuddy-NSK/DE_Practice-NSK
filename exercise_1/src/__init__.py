0bf60487fd9a9bd071062bab2a17fc10c32b5075
Nitin K <kendre.nitin2000@gmail.com>
2026-08-13 13:08:15 +0530

    update for importing easily
    added importing all functions from all files.

--- exercise_1/src/__init__.py
+++ exercise_1/src/__init__.py
@@ -1,5 +1,23 @@
-from async_data_ingestion import download_all_async, download_file_async
-from config import Config
-from data_ingestion import download_file
-from ingestion_helper import check_valid_url, delete_file, file_unzip, is_valid_member
-from utils import setup_logger
+# Copyright (c) 2024. All rights reserved.
+
+"""Data ingestion package for handling file downloads and processing.
+
+This package provides utilities for downloading, validating, and processing files,
+including both synchronous and asynchronous operations.
+"""
+
+from .async_data_ingestion import download_all_async, download_file_async
+from .config import Config
+from .data_ingestion import download_file
+from .ingestion_helper import check_valid_url, delete_file, file_unzip, is_valid_member
+
+__all__ = [
+    "Config",
+    "check_valid_url",
+    "delete_file",
+    "download_all_async",
+    "download_file",
+    "download_file_async",
+    "file_unzip",
+    "is_valid_member",
+]
